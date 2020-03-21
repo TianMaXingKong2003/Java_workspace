9c96c697d84efc4cacfbaffc50595cdca68c281b
Robert <31496215+TianMaXingKong2003@users.noreply.github.com>
2020-03-21 20:01:16 +0800

    2020_3_21

--- TomcatStudy/src/cn/itcast/session/SessionDemo2.java
+++ TomcatStudy/src/cn/itcast/session/SessionDemo2.java
@@ -9,22 +9,23 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
- * Servlet implementation class SessionDemo1
+ * Servlet implementation class SessionDemo2
  */
-@WebServlet("/SessionDemo1")
-public class SessionDemo1 extends HttpServlet {
+@WebServlet("/SessionDemo2")
+public class SessionDemo2 extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 //		使用Session共享数据
 //		1.获取session
 		HttpSession session = request.getSession();
-//		2.存储数据
-		session.setAttribute("message", "你好Session");
+//		2.获取数据
+		Object msg = session.getAttribute("message");
+		System.out.println(msg);
 		
 	}
 
-
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		this.doGet(request, response);
 	}
